f4880ae39921e26d270e1cc014a336760704e025
S M Kaiser Ahmed <53963906+mekaiser@users.noreply.github.com>
2021-05-13 22:23:01 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,32 @@
+// falsy values
+// 0
+// false
+// ""
+// undefined
+// null
+// NaN
+
+//---------------------------------------
+
+// true values
+//'0'
+// [] - empty array
+// {} - empty object\
+//'false'
+
+let name = 0;
+console.log(name); // undefined value, that's why condition is false
+
+// if(name){ // age != 0 then true, age = 0 then false
+//     console.log('condition is true');
+// }
+// else{
+//     console.log('condition is false');
+// }
+
+if(name || name == 0){ // age != 0 then true, age = 0 then false
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
